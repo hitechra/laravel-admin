1f9c0a35013096d8abbf0d29ce43f6366014ecbb
Adeardo Dora Harnanda <adetxt151@gmail.com>
2021-11-09 19:49:39 +0700

    WIP simple pagination

--- resources/views/simplePagination.blade.php
+++ resources/views/simplePagination.blade.php
@@ -6,24 +6,7 @@
     <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
     @endif
 
-    <!-- Pagination Elements -->
-    @foreach ($elements as $element)
-    <!-- "Three Dots" Separator -->
-    @if (is_string($element))
-    <li class="page-item disabled"><span class="page-link">{{ $element }}</span></li>
-    @endif
-
-    <!-- Array Of Links -->
-    @if (is_array($element))
-    @foreach ($element as $page => $url)
-    @if ($page == $paginator->currentPage())
-    <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
-    @else
-    <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
-    @endif
-    @endforeach
-    @endif
-    @endforeach
+    <li class="page-item"><span class="page-link">&nbsp;</span></li>
 
     <!-- Next Page Link -->
     @if ($paginator->hasMorePages())
